23be1ec4e8bf6b198ce99e61c64e627dbb0ef692
Vinicius Gomes <vinicius.gomes@modalgr.com.br>
2023-06-21 11:58:35 -0300

    (feat): footer finalizado

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,149 @@
 import { memo } from "react";
-import { FooterContainer, GridImages, ImageRadius } from "./style";
+import {
+  Container,
+  FooterContainer,
+  GridFooterInfos,
+  GridFooterLinks,
+  GridIconInfos,
+  GridIcons,
+  ImageRadius,
+  LinkFlex,
+  Links,
+  LocationInfos,
+} from "./style";
 import Image from "next/image";
-import produto1 from '../../assets/footer/footer1.png'
-import produto2 from '../../assets/footer/footer2.png'
-import produto3 from '../../assets/footer/footer3.png'
-import produto4 from '../../assets/footer/footer4.png'
+import produto1 from "../../assets/footer/footer1.png";
+import produto2 from "../../assets/footer/footer2.png";
+import produto3 from "../../assets/footer/footer3.png";
+import produto4 from "../../assets/footer/footer4.png";
+import logoFooter from "../../assets/footer/logoFooter.png";
+import IconsFooter from "../../assets/footer/IconsFooter.png";
 
 const component = () => {
   return (
     <FooterContainer>
-      <h1>@grãodegente</h1>
-      <GridImages>
-        <ImageRadius src={produto1} alt="" height={220}/>
-        <ImageRadius src={produto2} alt="" height={220}/>
-        <ImageRadius src={produto3} alt="" height={220}/>
-        <ImageRadius src={produto4} alt="" height={220}/>
-      </GridImages>
+      <Container>
+        <h1>@grãodegente</h1>
+        <GridFooterLinks>
+          <div>
+            <ImageRadius src={produto1} alt="" height={220} />
+            <h3>Institucional</h3>
+            <LinkFlex>
+              <Links href="a-empresa">A Empresa</Links>
+              <Links href="nosso-processo-de-producao">
+                Processo de Produção
+              </Links>
+              <Links href="nosso-processo-de-producao#controle-de-qualidadeo">
+                Controle de Qualidade
+              </Links>
+              <Links href="nosso-algodao">Nosso Algodão</Links>
+              <Links href="CentraldeAtendimento">Política de Privacidade</Links>
+              <Links href="CentraldeAtendimento/SupplierContact">
+                Seja Nosso Fornecedor
+              </Links>
+            </LinkFlex>
+          </div>
+          <div>
+            <ImageRadius src={produto2} alt="" height={220} />
+            <h3>Atendimento ao Cliente</h3>
+            <LinkFlex>
+              <Links href="a-empresa">A Empresa</Links>
+              <Links href="nosso-processo-de-producao">
+                Processo de Produção
+              </Links>
+              <Links href="nosso-processo-de-producao#controle-de-qualidadeo">
+                Controle de Qualidade
+              </Links>
+              <Links href="nosso-algodao">Nosso Algodão</Links>
+              <Links href="CentraldeAtendimento">Política de Privacidade</Links>
+              <Links href="CentraldeAtendimento/SupplierContact">
+                Seja Nosso Fornecedor
+              </Links>
+            </LinkFlex>
+          </div>
+          <div>
+            <ImageRadius src={produto3} alt="" height={220} />
+            <h3>Ajuda</h3>
+            <LinkFlex>
+              <Links href="a-empresa">A Empresa</Links>
+              <Links href="nosso-processo-de-producao">
+                Processo de Produção
+              </Links>
+              <Links href="nosso-processo-de-producao#controle-de-qualidadeo">
+                Controle de Qualidade
+              </Links>
+              <Links href="nosso-algodao">Nosso Algodão</Links>
+              <Links href="CentraldeAtendimento">Política de Privacidade</Links>
+              <Links href="CentraldeAtendimento/SupplierContact">
+                Seja Nosso Fornecedor
+              </Links>
+            </LinkFlex>
+          </div>
+          <div>
+            <ImageRadius src={produto4} alt="" height={220} />
+            <h3>Certificado</h3>
+            <LinkFlex>
+              <Links href="a-empresa">A Empresa</Links>
+              <Links href="nosso-processo-de-producao">
+                Processo de Produção
+              </Links>
+              <Links href="nosso-processo-de-producao#controle-de-qualidadeo">
+                Controle de Qualidade
+              </Links>
+              <Links href="nosso-algodao">Nosso Algodão</Links>
+              <Links href="CentraldeAtendimento">Política de Privacidade</Links>
+              <Links href="CentraldeAtendimento/SupplierContact">
+                Seja Nosso Fornecedor
+              </Links>
+            </LinkFlex>
+          </div>
+        </GridFooterLinks>
+        <GridFooterInfos>
+          <div>
+            <Image src={logoFooter} alt="logo rodapé" />
+            <LocationInfos>
+              <p>
+                Grão de Gente - LGF Comércio Eletrônico LTDA -
+                CNPJ:26.384.531/0001-19
+              </p>
+              <p>
+                Rod mg 290, Km 73, s/n - Vargem da Forquilha - Jacutinga/MG -
+                CEP:37.590-000
+              </p>
+            </LocationInfos>
+          </div>
+          <GridIconInfos>
+            <div>
+              <h3>Siga a Grão:</h3>
+            </div>
+            <GridIcons>
+              <div>
+                <Links href="https://www.instagram.com/">
+                  <Image src={IconsFooter} alt="" />
+                </Links>
+                <Links href="https://www.instagram.com/">
+                  <Image src={IconsFooter} alt="" />
+                </Links>
+                <Links href="https://www.instagram.com/">
+                  <Image src={IconsFooter} alt="" />
+                </Links>
+              </div>
+              <div>
+                <Links href="https://www.instagram.com/">
+                  <Image src={IconsFooter} alt="" />
+                </Links>
+                <Links href="https://www.instagram.com/">
+                  <Image src={IconsFooter} alt="" />
+                </Links>
+                <Links href="https://www.instagram.com/">
+                  <Image src={IconsFooter} alt="" />
+                </Links>
+              </div>
+            </GridIcons>
+          </GridIconInfos>
+        </GridFooterInfos>
+      </Container>
     </FooterContainer>
-  )
+  );
 };
-
 export const Footer = memo(component);
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,20 +1,115 @@
 import { styled } from "@stitches/react";
 import Image from "next/image";
+import Link from "next/link";
 
-export const FooterContainer = styled('footer',{
-  width: '100%',
-  background: "$blue"
-})
+export const FooterContainer = styled("main", {
+  width: "100%",
+  background: "$blue",
 
-export const GridImages = styled('div',{
-  display: 'grid',
-  gridTemplateColumns: 'repeat(4, 1fr)',
-  justifyContent: 'center',
-  width: '30%',
-  margin: '0 auto',
-  gap: '1rem',
-})
+  h1: {
+    display: "flex",
+    justifyContent: "center",
+    color: "$white",
+    fontFamily: "serif",
+    padding: "2.5rem 0 1.5rem 0",
+    fontSize: "$xl",
+    fontWeight: "bold",
+  },
+});
 
-export const ImageRadius = styled(Image ,{
-  borderRadius: '1rem',
-})
+export const Container = styled("footer", {
+  width: "80%",
+  margin: "0 auto",
+});
+
+export const GridFooterLinks = styled("div", {
+  display: "grid",
+  gridTemplateColumns: "repeat(4, 1fr)",
+  justifyContent: "center",
+  width: "100%",
+  margin: "0 auto",
+  gap: "3rem",
+
+  h3: {
+    color: "$white",
+    fontFamily: "sans-serif",
+    fontSize: "$md",
+    paddingBottom: "1.5rem",
+  },
+
+  div: {
+    padding: "5px",
+  },
+});
+
+export const ImageRadius = styled(Image, {
+  borderRadius: "1rem",
+  marginBottom: "3rem",
+});
+
+export const LinkFlex = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+});
+
+export const Links = styled(Link, {
+  textDecoration: "none",
+  color: "$white",
+  fontFamily: "sans-serif",
+
+  "&:hover": {
+    transition: "all 0.2s ease-in-out",
+    opacity: 0.8,
+    cursor: "pointer",
+  },
+});
+
+export const GridFooterInfos = styled("div", {
+  borderTop: "1px solid #68C9BE",
+  display: "grid",
+  gridTemplateColumns: "1.5fr 1fr",
+  width: "100%",
+  margin: "1rem auto",
+  padding: "2rem 0",
+
+  div: {
+    display: "flex",
+    alignItems: "center",
+  },
+});
+
+export const LocationInfos = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  fontFamily: "serif",
+  color: "$white",
+  justifyContent: "center",
+
+  p: {
+    display: "flex",
+    justifyContent: "initial",
+    fontSize: "14px",
+  },
+});
+
+export const GridIconInfos = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "1rem",
+
+  h3: {
+    fontSize: "$xl",
+    color: "$white",
+    fontFamily: "ui-sans-serif",
+    display: "flex",
+    alignItems: "center",
+    fontWeight: 'normal'
+  },
+});
+
+export const GridIcons = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+});
